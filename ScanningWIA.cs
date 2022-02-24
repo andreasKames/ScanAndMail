d1fa8e17fa94711ced5dc187765ba1182e4e346f
Andreas <andreas_kames@yahoo.de>
2022-02-24 11:39:54 +0100

    Methode ListScanner implementiert

--- ScanningWIA.cs
+++ ScanningWIA.cs
@@ -12,9 +12,34 @@ namespace ScanAndMail
         private DeviceManager deviceManager;
         private DeviceInfo deviceInfo;
 
-        void chooseScanner()
+        ScanningWIA()
         {
+            deviceManager = new DeviceManager();
+        }
 
+        List<String> ListScanner()
+        {
+            List<String> scanner = new List<String>();
+            
+            // Loop through the list of devices
+            // ATTENTION: deviceManager.DeviceInfos[i] starts with 1 !!!!
+            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+            {
+                // Skip the device if it's not a scanner
+                if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                {
+                    continue;
+                }
+                scanner.Add( (string)deviceManager.DeviceInfos[i].Properties["Name"].get_Value());
+            }
+            /*
+            if (deviceManager.DeviceInfos.Count > 0)
+            {
+                ListBoxScanner.SelectedIndex = 0;
+
+            }
+            */
+            return scanner;
         }
 
         void scanImage(String fileName)
