f8b9df3ab2b2df3c682be0b345ecf99224a559fa
Andreas <andreas_kames@yahoo.de>
2022-02-26 11:47:41 +0100

    kleine Veränderungen bei Klasse Configuration

--- Configuaratiion.cs
+++ Configuaratiion.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,28 @@ namespace ScanAndMail
 {
     internal class Configuaratiion
     {
+        private int scannerNumber { get; set; }
+        private String path { get; set; }
+        private String fileName  { get; set; }
+
+
+        public void setConf()
+        {
+            Console.WriteLine(string.Format("'{0}' project is created in '{1}' language ", 1, 2));
+        }
+
+        public void ReadConfFromFile()
+        {
+            scannerNumber = Convert.ToInt32( ConfigurationManager.AppSettings["ScannerNumber"]);
+            path = ConfigurationManager.AppSettings["Path"];
+            fileName = ConfigurationManager.AppSettings["FileName"];
+        }
+
+        public  void SaveConfToFile()
+        {
+
+        }
+        
+
     }
 }
